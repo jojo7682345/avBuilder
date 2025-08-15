b5de9c9d5135451d3d5bc3d2e839f4132a5f4b2d
jojo7682345 <jojo7682345@gmail.com>
2025-08-16 00:07:36 +0200

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "avBuilder - A flexible build system written in C";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.avbuilder = pkgs.stdenv.mkDerivation rec {
+        pname = "avBuilder";
+
+        # Fetch the repo and preserve .git for version calculation
+        src = pkgs.fetchgit {
+          url = "https://github.com/jojo7682345/avBuilder.git";
+		  sha256 = "sha256-kuYzapckWMzGS/2rb+4blJ4LlaKa20XGkmGn1+GwwZA="; # Replace with actual hash
+        };
+
+        # Compute version from git commit count and short hash
+		version = "v0.1n";	
+
+        nativeBuildInputs = [ pkgs.git pkgs.gcc ];
+
+        buildPhase = ''
+          chmod +x ./bootstrap
+          ./bootstrap
+          ./avBuilder avBuilder.project
+        '';
+
+        installPhase = ''
+          ./bootstrap install $out/bin/avBuilder
+
+		  # Install your default projects somewhere inside the store
+  		  mkdir -p $out/share/avBuilder/library/std/c
+  		  cp ./library/c/stdc.project $out/share/avBuilder/library/std/c/
+
+  		  mkdir -p $out/share/avBuilder/library/std/project
+  		  cp ./library/project/import.project $out/share/avBuilder/library/std/project/	
+        '';
+	
+		shellHook = ''
+			export AVBUILDER_HOME=$out/share/avBuilder
+		'';
+	
+        meta = with pkgs.lib; {
+          description = "A flexible build system written in C";
+          homepage = "https://github.com/jojo7682345/avBuilder";
+          license = licenses.mit;
+          maintainers = with maintainers; [ ];
+        };
+      };
+
+	  # Set the default package for `nix run .`
+      defaultPackage.${system} = self.packages.${system}.avbuilder;
+
+      # Set up a devShell for `nix develop .`
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ self.packages.${system}.avbuilder ];
+        shellHook = ''
+          export AVBUILDER_HOME=${self.packages.${system}.avbuilder}/share/avBuilder
+        '';
+      };
+    };
+}
+
